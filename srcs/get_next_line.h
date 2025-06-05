7ed266354a9c4a16d8ae70bb50be1fdd5e463653
Olaf Kuilboer <kuilboer@gmail.com>
2025-06-05 10:49:52 +0200

    Intial commit.

---
+++ srcs/get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/04 22:26:23 by okuilboe      #+#    #+#                 */
+/*   Updated: 2025/06/04 22:51:54 by okuilboe      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+
+
+#endif
---
+++ srcs/get_next_line_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_utils.c                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/04 22:38:12 by okuilboe      #+#    #+#                 */
+/*   Updated: 2025/06/05 10:36:26 by okuilboe      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/**
+ * This file stores all the helper functions for get_next_line
+ */
+
+ /**
+  * @brief Append memory to the sting variable line with 'len' bytes. 
+  */
+ void *reallocate(char *line,size_t len)
+ {
+	/**
+	 * 
+	 */
+ }
